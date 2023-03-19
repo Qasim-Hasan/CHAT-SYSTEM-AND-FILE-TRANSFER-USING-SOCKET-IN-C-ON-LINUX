b19159462c835728df7ce633ac7ae09bf9f76dc1
Qasim Hasan <94786002+pkcoder420@users.noreply.github.com>
2023-03-20 00:48:14 +0500

    To open multiple Terminals/Consoles Simultaneously

---
+++ Multiple Terminal Code.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#konsole --noclose -e ./server 3210 &
+#sleep 2
+#konsole --noclose -e ./client 127.0.0.1 3210 &
+#exit
+
+
+konsole --noclose -e ./file1 &
+sleep 2
+konsole --noclose -e ./file1 &
+exit
+
