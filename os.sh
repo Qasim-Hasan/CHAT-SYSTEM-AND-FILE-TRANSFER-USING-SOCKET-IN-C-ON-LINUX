ba546da3f04493c363c4f76d2fd6f880b732d615
Qasim Hasan <94786002+pkcoder420@users.noreply.github.com>
2023-05-04 02:02:56 +0500

    Update os.sh

--- os.sh
+++ os.sh
@@ -4,7 +4,8 @@ date "+DATE: %D%nTIME: %T"
 echo -e "\e[1;42m  a.) Group Chat Application (Multi-user/Multi-Threading) \e[0m"
 echo -e "\e[1;42m  b.) Client / Server (Calculator) \e[0m "
 echo -e "\e[1;42m  c.) Client / Server (File Transfering) \e[0m "
-echo -e "\e[1;42m  d.) Exit \e[0m" 
+echo -e "\e[1;42m  d.) Single Client / Server \e[0m "
+echo -e "\e[1;42m  e.) Exit \e[0m" 
 
 read -p "Enter Choice(a to d):" choice
 
@@ -27,6 +28,12 @@ case $choice in
                 cd /home/qasim/Desktop/OS_PROJECT/File_Transfer
                 $SHELL
                 ;;
-        "d"|"d")
+        "d"|"D")
+                echo -e "\e[1;42m Single Client/Server....\e[0m"
+                sleep 2
+                cd /home/qasim/Desktop/OS_PROJECT/Single_Client_to_Server
+                $SHELL
+               ;;
+        "e"|"E")
                 echo -e "\e[1;41m Program Terminated \e[0m" 
 esac
